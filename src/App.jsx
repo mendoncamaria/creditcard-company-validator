751a9325e84090acb0a2cd260c982474605bae8b
Maria Mendonca <maria.mendonca156@gmail.com>
2023-09-27 15:17:21 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CreditCardValidator from './CardValidator'
+
+function App() {
+
+  return (
+    <>
+      <CreditCardValidator />
+    </>
+  )
+}
+
+export default App
